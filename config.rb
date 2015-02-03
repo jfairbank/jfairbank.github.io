743d570f4ed89bf9a799a4727cdf62456a8964e6
Jeremy Fairbank <elpapapollo@gmail.com>
2015-02-03 00:58:21 -0500

    Lawl forgot to minify assets

--- config.rb
+++ config.rb
@@ -45,7 +45,7 @@ end
 
 # Reload the browser automatically whenever files change
 configure :development do
- activate :livereload
+  activate :livereload
 end
 
 # Methods defined in the helpers block are available in templates
@@ -74,13 +74,13 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
